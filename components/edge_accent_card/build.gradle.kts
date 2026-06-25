8845f391a73622acf3746c9e2bebe467159cafce
Sai-Gopal-08 <86779443+Sai-Gopal-08@users.noreply.github.com>
2026-06-25 11:44:58 +0530

    created: release version for components

--- components/edge_accent_card/build.gradle.kts
+++ components/edge_accent_card/build.gradle.kts
@@ -13,8 +13,19 @@ android {
 
     defaultConfig {
         minSdk = 24
+        consumerProguardFiles("consumer-rules.pro")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
